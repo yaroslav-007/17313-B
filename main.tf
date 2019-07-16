7f94928a47bf0c1411d77948c066093184798ebb
Yaroslav <yorimark@abv.bg>
2019-07-16 12:59:48 +0300

    update files

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "remote" {
+    organization = "yaroslav-org"
+
+    workspaces {
+      name = "Workspace-B"
+    }
+  }
+}
+
+
+
+data "terraform_remote_state" "workspace-a" {
+  backend = "remote"
+
+  config = {
+    organization = "yaroslav-org"
+    workspaces = {
+      name = "Workspace-A"
+    }
+  }
+
+}
+
+
+
+
+
+resource "null_resource" "HelloString" {
+  provisioner "local-exec" {
+    command = "echo Hello ${data.terraform_remote_state.workspace-a.outputs.sluchaen_string}"
+  }
+}
+
